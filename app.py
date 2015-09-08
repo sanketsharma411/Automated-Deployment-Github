a0e2ae6aa3923481ab3a17b43af128de8a65a523
Sanket Sharma <sanketsharma411@gmail.com>
2015-09-08 16:04:13 -0400

    Fixed request validation

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ def home():
 @app.route('/postreceive/', methods = ['POST','GET'])
 def postreceive():
     if request.method == 'POST':
-        
-        return "Data Validated"
+        try: 
+            validate_postreceive_hook(request.data)
+        except ValueError,e:
+            return str(e)
+        return request.data
     else:
         return "Invalid METHOD %s" %str(request.method)
         
@@ -22,11 +25,9 @@ with open('app_data.json','r') as f:
     app_data = json.load(f)
     
 def validate_postreceive_hook(data):
-    if data['html_url'] == app_data['html_url'] :
-        return None
-    else:
-        raise ValueError('Invalid data')
-    
+    if data['repository']['html_url'] != app_data['html_url'] :
+        raise ValueError('Invalid Repository URL')
+
 # If called from command line run in Flask development server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9999)
